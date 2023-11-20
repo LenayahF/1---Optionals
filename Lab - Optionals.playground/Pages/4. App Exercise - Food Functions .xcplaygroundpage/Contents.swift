7aa80d12407405d04f1927dc32c6d66c3004adcb
Lenayah <lfanesie@gmail.com>
2023-11-20 12:10:09 -0500

    update

--- Lab - Optionals.playground/Pages/4. App Exercise - Food Functions .xcplaygroundpage/Contents.swift
+++ Lab - Optionals.playground/Pages/4. App Exercise - Food Functions .xcplaygroundpage/Contents.swift
@@ -15,6 +15,13 @@ struct Meal {
 
 var meals: [String: Meal] = ["Breakfast": Meal(food: ["Bagel", "Orange Juice", "Egg Whites"], calories: 530)]
 
+func getMeal(meal: String)-> Meal?{
+   
+    return meals[meal]
+}
+
+print(getMeal(meal: "Lunch"))
+print(getMeal(meal: "Breakfast"))
 
 /*:
  iOS comes with a few different APIs for persistence, or saving data. You'll learn more about persistence in another lesson, but for now imagine what an app experience would be like if every time you opened the app all of your data was gone. That would be frustrating, right?
